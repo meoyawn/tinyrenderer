29fb77fec8d094f914021286d315b08efe8f148a
adelnizamutdinov <stiggpwnz@gmail.com>
2016-05-18 19:29:06 +0300

    african head

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate byteorder;
+extern crate wavefront_obj;
+extern crate core;
 
 mod tga;
 mod renderer;
@@ -6,6 +8,8 @@ mod renderer;
 use tga::{TgaColor, TgaImage};
 use std::fs::File;
 use renderer::line;
+use wavefront_obj::obj::{parse,ObjSet};
+use std::io::Read;
 
 const WHITE: TgaColor = TgaColor { rgba: [255, 255, 255, 255] };
 const RED: TgaColor = TgaColor { rgba: [255, 0, 0, 255] };
@@ -17,6 +21,15 @@ fn main() {
     line(20, 13, 40, 80, &mut image, RED);
     line(80, 40, 13, 20, &mut image, RED);
 
+    println!("{:?}", head().objects[0].geometry[0].shapes.len());
+
     let mut f = File::create("foo.tga").unwrap();
     image.write(&mut f).unwrap();
+}
+
+fn head() -> ObjSet {
+let mut input = String::new();
+let mut f =     File::open("obj/african_head.obj").unwrap();
+f.read_to_string(&mut input).unwrap();
+parse(input).unwrap()
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,9 +1,44 @@
 use tga::{TgaImage, TgaColor};
+use std::mem::swap;
 
 pub fn line(x0: u16, y0: u16, x1: u16, y1: u16, image: &mut TgaImage, color: TgaColor) -> () {
-    for x in x0..x1 + 1 {
-        let t = (x - x0) as f32 / (x1 - x0) as f32;
-        let y = y0 as f32 * (1f32 - t) + y1 as f32 * t;
-        image.set(x as usize, y as usize, color);
+    let mut steep = false;
+
+    let mut finx0 = x0 as i32;
+    let mut finx1 = x1 as i32;
+    let mut finy0 = y0 as i32;
+    let mut finy1 = y1 as i32;
+
+    if (x0 as i32 - x1 as i32).abs() < (y0 as i32 - y1 as i32).abs() {
+        swap(&mut finx0, &mut finy0);
+        swap(&mut finx1, &mut finy1);
+        steep = true;
+    }
+    if finx0 > finx1 {
+        swap(&mut finx0, &mut finx1);
+        swap(&mut finy0, &mut finy1);
+    }
+
+    let dx = finx1 - finx0;
+    let dy = finy1 - finy0;
+    let derror2 = dy.abs() * 2;
+    let mut error2 = 0;
+    let mut y = finy0;
+
+    for x in finx0..finx1 + 1 {
+        if steep {
+            image.set(y as usize, x as usize, color);
+        } else {
+            image.set(x as usize, y as usize, color);
+        }
+        error2 += derror2;
+        if error2 > dx {
+            y += if y1 > y0 {
+                1
+            } else {
+                -1
+            };
+            error2 -= dx * 2;
+        }
     }
 }
